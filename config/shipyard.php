df6f0960c01db6b4ac269da577cb718c351f201d
dubuqingfeng <1135326346@qq.com>
2016-07-30 17:22:09 +0800

    Add: config/shipyard.php

---
+++ config/shipyard.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 16-7-30
+ * Time: 5:12
+ */
+
+return [
+    'default' => 'main',
+    'connections' => [
+        'main' => [
+            'id' => 'your-application-id',
+            'key' => 'your-api-key',
+        ],
+    ],
+];
